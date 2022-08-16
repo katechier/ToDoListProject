6253cdd3f9729eb4b42ef11817c5d7e86eaef15a
Kate <katechier15@gmail.com>
2022-08-16 14:25:54 -0700

    Initial Commit

---
+++ ToDoListProject/ToDo.swift
@@ -0,0 +1,13 @@
+//
+//  ToDo.swift
+//  ToDoListProject
+//
+//  Created by Kate Chier on 8/15/22.
+//
+
+import UIKit
+
+class ToDo {
+    var name = ""
+    var important = false
+}
---
+++ ToDoListProject/ToDoTableViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ToDoTableViewController.swift
+//  ToDoListProject
+//
+//  Created by Kate Chier on 8/15/22.
+//
+
+import UIKit
+
+class ToDoTableViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
